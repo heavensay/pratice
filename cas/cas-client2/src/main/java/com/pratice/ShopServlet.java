c8cc7fad327c11d9e098e9477c23b3325faa47d3
zny818@163.com <zny818@163.com>
2018-01-16 14:17:07 +0800

    cas-client   init

---
+++ cas/cas-client2/src/main/java/com/pratice/ShopServlet.java
@@ -0,0 +1,16 @@
+package com.pratice;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShopServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().write("welcome to cas-client 2");
+	}
+}
