4f14ea8b709e3bba88bc40f490ad96f934b41cb3
zny818@163.com <zny818@163.com>
2018-05-20 23:05:38 +0800

    activity init

---
+++ oa-activiti/src/main/webapp/page/404.vue
@@ -0,0 +1,19 @@
+	
+<template id="not-found">	
+	<div>
+		<span slot="content"></span>
+	</div>
+</template>
+	
+<style>
+	
+</style>
+
+<script type="text/javascript">
+
+	Vue.component('NotFoundConponent',{
+		template:'#not-found'
+	})
+
+</script>
+
